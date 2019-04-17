76ba75ea8139bde401f8be99967e482577747903
Dito W <morpig2@gmail.com>
2019-04-17 15:16:56 +0700

    update

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ let T = new Twit({
 let teamJokowi = 0;
 let teamPrabowo = 0;
 
+let jokowi = 0;
+let prabowo = 0;
+let pemilu = 0;
+
 let jkwStream = T.stream('statuses/filter', { track: '#JokoWinElection'} );
 
 jkwStream.on('tweet', (tweet) => {
@@ -31,9 +35,36 @@ prbStream.on('tweet', (tweet) => {
   console.log(`${new Date()}: PRB ${tweet.user.screen_name}(${tweet.user.id}) ${tweet.text} (${tweet.retweet_count}/${tweet.favorite_count})`)
 })
 
+let jokowiStream = T.stream('statuses/filter', { track: 'jokowi'} );
+
+jokowiStream.on('tweet', (tweet) => {
+  jokowi = jokowi + 1
+  console.log(`${new Date()}: JKW ${tweet.user.screen_name}(${tweet.user.id}) ${tweet.text} (${tweet.retweet_count}/${tweet.favorite_count})`)
+})
+
+let prabowoStream = T.stream('statuses/filter', { track: 'prabowo'} );
+
+prabowoStream.on('tweet', (tweet) => {
+  prabowo = prabowo + 1
+  console.log(`${new Date()}: PRB ${tweet.user.screen_name}(${tweet.user.id}) ${tweet.text} (${tweet.retweet_count}/${tweet.favorite_count})`)
+})
+
+let pemiluStream = T.stream('statuses/filter', { track: '#Pemilu2019'} );
+
+pemiluStream.on('tweet', (tweet) => {
+  pemilu = pemilu + 1
+  console.log(`${new Date()}: PEMILU ${tweet.user.screen_name}(${tweet.user.id}) ${tweet.text} (${tweet.retweet_count}/${tweet.favorite_count})`)
+})
+
 setInterval(() => {
-  metrics.gauge(`stream.jkw`, teamJokowi);
-  metrics.gauge(`stream.prb`, teamPrabowo);
+  metrics.gauge(`stream.teamJkw`, teamJokowi);
+  metrics.gauge(`stream.teamPrb`, teamPrabowo);
+  metrics.gauge(`stream.jkw`, jokowi);
+  metrics.gauge(`stream.prb`, prabowo);
+  metrics.gauge(`stream.pemilu`, pemilu);
+  jokowi = 0;
+  prabowo = 0;
+  pemilu = 0;
   teamJokowi = 0;
   teamPrabowo = 0;
 }, 120000) //every 5 minutes report to datadog
