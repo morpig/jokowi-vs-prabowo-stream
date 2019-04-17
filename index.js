9f7398ca02e1eb8cad6238da939e1be9f60da973
Dito W <morpig2@gmail.com>
2019-04-17 15:33:53 +0700

    fixx

--- index.js
+++ index.js
@@ -16,19 +16,16 @@ let T = new Twit({
 
 let teamJokowi = 0;
 let teamPrabowo = 0;
-
-let jokowi = 0;
-let prabowo = 0;
 let pemilu = 0;
 
-let jkwStream = T.stream('statuses/filter', { track: '#JokoWinElection'} );
+let jkwStream = T.stream('statuses/filter', { track: ['#JokoWinElection', 'jokowi', 'maaruf', '01']} );
 
 jkwStream.on('tweet', (tweet) => {
   teamJokowi = teamJokowi + 1
   console.log(`${new Date()}: JKW ${tweet.user.screen_name}(${tweet.user.id}) ${tweet.text} (${tweet.retweet_count}/${tweet.favorite_count})`)
 })
 
-let prbStream = T.stream('statuses/filter', { track: '#TheVictoryOfPrabowo'} );
+let prbStream = T.stream('statuses/filter', { track: ['#TheVictoryOfPrabowo', 'prabowo', 'sandiaga', '02']} );
 
 prbStream.on('tweet', (tweet) => {
   teamPrabowo = teamPrabowo + 1
@@ -45,11 +42,7 @@ pemiluStream.on('tweet', (tweet) => {
 setInterval(() => {
   metrics.gauge(`stream.teamJkw`, teamJokowi);
   metrics.gauge(`stream.teamPrb`, teamPrabowo);
-  metrics.gauge(`stream.jkw`, jokowi);
-  metrics.gauge(`stream.prb`, prabowo);
   metrics.gauge(`stream.pemilu`, pemilu);
-  jokowi = 0;
-  prabowo = 0;
   pemilu = 0;
   teamJokowi = 0;
   teamPrabowo = 0;
